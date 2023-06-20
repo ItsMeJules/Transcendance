5ade46b031bf89e6d5f704f25eef886f1d0faf9e
ItsMeJules <jules.prn06@gmail.com>
2023-06-20 16:26:11 +0200

    Started playing with the db

---
+++ back/prisma/migrations/20230620142340_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_friends" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_friends_AB_unique" ON "_friends"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_friends_B_index" ON "_friends"("B");
+
+-- AddForeignKey
+ALTER TABLE "_friends" ADD CONSTRAINT "_friends_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_friends" ADD CONSTRAINT "_friends_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ back/src/user/service/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+// import { User, Prisma } from '@prisma/client';
+
+import { PrismaService } from "src/database/prisma.service";
+
+@Injectable()
+export class UserService {
+	constructor(private prismaService : PrismaService) {}
+
+	// async createUser(data : Prisma.UserCreateInput): Promise<User> {
+	// 	return this.prismaService.user.create({
+	// 		data,
+	// 	});
+	// }
+}
