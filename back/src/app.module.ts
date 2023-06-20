e79189fccc0269f5cddcc21263727a24d38af8a9
ItsMeJules <jules.prn06@gmail.com>
2023-06-20 15:41:02 +0200

    removed unused files

--- back/src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config'
-import * as Joi from '@hapi/joi';
-
-export class AppModule {}
