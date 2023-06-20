03c1a736731addd40ca45eb5b4b5a0178ea71717
ItsMeJules <jules.prn06@gmail.com>
2023-06-20 15:39:28 +0200

    removed unused packages

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,23 +4,4 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config'
 import * as Joi from '@hapi/joi';
 
-import { DatabaseModule } from './database/database.module';
-
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        POSTGRES_HOST: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
-        PORT: Joi.number(),
-      })
-    }),
-    DatabaseModule
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})
 export class AppModule {}
