b624ea85d979d44d0f242e499ca86a7f106e9de2
ItsMeJules <jules.prn06@gmail.com>
2023-07-28 16:29:45 +0200

    infos pour antoine

--- back/src/chat/chat.gateway.ts
+++ back/src/chat/chat.gateway.ts
@@ -17,6 +17,21 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 	@WebSocketServer()
 	server: Server;
 
+	/*			 INFO POUR LE FRONT
+
+		Un id unique du client
+			- Le channel actuel du client (publics, prives, proteges par mdp)
+			- Les channels auxquel il a acces
+			- Users bloques
+
+				INFO POUR LE BACK
+		Un historique des messages :
+			- bdd-row: | l'heure du msg | (PK) id | (FK) userId | (FK) channel | contenu du msg
+			- Broadcast les donnees aux bons utilisateurs
+			- Log
+	*/
+	
+
 	handleConnection(client: Socket) {
 		console.log("Client connected: ", client.id)
 	}
@@ -27,7 +42,7 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
 	@SubscribeMessage('message')
 	handleEvent(@MessageBody() data: string) {
-		this.server.emit('message', data)	
+		this.server.emit('message', data)
 	}
 
 }
