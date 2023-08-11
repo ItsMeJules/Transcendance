6a5182163b13e378c9ccdafdee49dcdf47d5fffb
Jules <jules.prn06@gmail.com>
2023-08-11 18:39:59 +0200

    Need to fix channel password not setting right place

--- front/src/chat/metadata/more/ChannelCreationPopup.jsx
+++ front/src/chat/metadata/more/ChannelCreationPopup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Popup from "./Popup"
 
@@ -7,15 +7,22 @@ import PrivateIcon from "../../../assets/private.png"
 import ProtectedIcon from "../../../assets/padlock.png"
 
 export default function ChannelCreationPopup() {
+  const [passwordInput, setPasswordInput] = useState(false)
 
   return (
     <Popup className="channel-creation-popup">
       <input placeholder="Nom du channel" required />
+
       <div className="icons">
-        <img src={PublicIcon}></img>
-        <img src={PrivateIcon}></img>
-        <img src={ProtectedIcon}></img>
+        <img src={PublicIcon} alt="Public"></img>
+        <img src={PrivateIcon} alt="Private"></img>
+        <img src={ProtectedIcon} alt="Protected" onClick={() => setPasswordInput(!passwordInput)}></img>
       </div>
+
+      {passwordInput &&
+        <div className="password-input">
+          <input placeholder="Entrez le mdp du channel." />
+        </div>}
     </Popup>
   )
 
