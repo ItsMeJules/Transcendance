9f77e4dd201ffe7f16b2f5e0319b0ef0d630758a
Antoine COUSINIE <acousini@paul-f4Br9s2.clusters.42paris.fr>
2023-08-17 09:19:54 +0200

    delete chat folder

--- front/src/chat/metadata/more/Popup.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-export default function Popup({ className, children }) {
-	return (
-		<div className={className} onClick={(e) => e.stopPropagation()}>
-			{children}
-		</div>
-	)
-}
