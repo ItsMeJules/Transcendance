573d243b6002272e8dd381afad5cf5c205b06b22
hopscott <scott.willis2706@gmail.com>
2023-08-30 20:12:09 +0200

    migrated new db schema

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from "utils/redux/Store";
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import 'App.css';
 import Websocket from 'services/Websocket/Websocket';
@@ -14,7 +18,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path={APP_ROUTES.SIGN_IN} element={<LayoutWrapper><Signin /></LayoutWrapper>} />
         <Route path={APP_ROUTES.SIGN_UP} element={<LayoutWrapper><Signup /></LayoutWrapper>} />
-        <Route path={'/dashboard'} element={<Websocket><LayoutWrapper><Dashboard /></LayoutWrapper></Websocket>} />
+        <Route path={'/dashboard'} element={<Provider store={store}><PersistGate persistor={persistor}><Websocket><LayoutWrapper><Dashboard /></LayoutWrapper></Websocket></PersistGate></Provider>} />
       </Routes>
     </ >
   );
